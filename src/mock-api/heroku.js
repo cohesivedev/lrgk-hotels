6259c2c77f2ee9c2d4d3ddcb47377417a920c709
jsyang <jsyang.ca@gmail.com>
2019-03-15 00:53:47 +0000

    Fix heroku data-source and do a release build for github pages

--- src/mock-api/heroku.js
+++ src/mock-api/heroku.js
@@ -2,5 +2,5 @@ const jsonServer = require('json-server');
 const server     = jsonServer.create();
 
 const DATA = require('.')();
-server.use((req, res) => res.json(DATA));
+server.use((req, res) => res.json(DATA.hotels));
 server.listen(process.env.PORT || 3001);
