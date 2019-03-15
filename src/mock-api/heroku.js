cb43afd9b46123a0cc7c040b38fc9d05421acbab
jsyang <jsyang.ca@gmail.com>
2019-03-15 01:04:10 +0000

    Allow CORS from heroku

--- src/mock-api/heroku.js
+++ src/mock-api/heroku.js
@@ -2,5 +2,9 @@ const jsonServer = require('json-server');
 const server     = jsonServer.create();
 
 const DATA = require('.')();
-server.use((req, res) => res.json(DATA.hotels));
+server.use((req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.json(DATA.hotels);
+});
 server.listen(process.env.PORT || 3001);
